f432eae4d9d63498d7ab41485525f8c51672c921
shyamgh <shyamgh@gmail.com>
2019-10-10 17:21:10 +0530

    added login api test and sample pkg1 using pytest

---
+++ Test-automation-proj1/tests/login_api_test/conftest.py
@@ -0,0 +1,11 @@
+'''
+Created on 10-Oct-2019
+
+@author: shushangabadkar
+'''
+
+import pytest
+
+@pytest.fixture
+def supply_url():
+    return "https://reqres.in/api"
---
+++ Test-automation-proj1/tests/pkg1/test_sample.py
@@ -0,0 +1,13 @@
+'''
+Created on 10-Oct-2019
+
+@author: shushangabadkar
+'''
+
+
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 5
